ab387ea874c8bef99c4c1d5af6d7e312722a22b5
Eduardo Tonatto <edtonatto@gmail.com>
2023-07-06 01:18:37 -0300

    :sparkles: Added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header'
 import './global.css'
 
 
 export function App() {
   return (
-    <strong>Hello World</strong>
+    <div>
+      <Header />
+    </div>
   )
 }
